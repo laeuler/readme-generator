24adae1fed1770396c4f0cc3f52e58c54a9557df
Lars Euler <eulerlars@gmail.com>
2021-11-21 16:01:46 +0100

    added content to each section in README

--- modules/readme.js
+++ modules/readme.js
@@ -15,26 +15,45 @@ const createReadme = (data) => {
   } = data;
 
   //Markdown from here
-  return `# ${title}
+  return `
+ 
+# ${title}
 
 ## Description
 ${description}
 
 ## Table of Content
+- [Description](#Description)
+- [Usage](#Usage)
+- [Installation](#Installation)
+- [License](#License)
+- [Contributors](#Contributors)
+- [Tests](#Tests)
+- [Questions](#Questions)
 
 ## Installation
-${instructions}
+\`\`\`${instructions}\`\`\`
 
 ## Usage
+${usage}
 
 ## License
+${license}
 
 ## Contributing
+Please reach out to ${githubname} for further information on how to contribute to this project.
+
+Contributors so far:
+${contributors}
 
 ## Tests
+${test}
+
 
 ## Questions
-
+In case of questions, please contact the creator via
+- GitHub: ${githubname}
+- Mail: ${usermail} (please use the following subject: ${title}, I will follow up as soon as possible)
 `;
 };
 
